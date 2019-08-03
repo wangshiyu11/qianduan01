9d7e7cb20c9d170123e1251a977e951846c7723b
wangshiyu11 <1017887438@qq.com>
2019-08-03 11:31:15 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import login  from '@/view/login/login'
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login,
+      meta:{
+        require:false//该路径的访问不需要登录
+      }
+    }/*,{
+      path: '/error',
+      name: 'error',
+      component: error,
+      meta:{
+        require:false//该路径的访问不需要登录
+      }
+    }*/,
+
+  ]
+})
